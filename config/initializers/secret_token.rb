bf5db43f9abbaf48da91a0cbf7037c985491e5d6
johnnie ho <smallon08@yahoo.com.hk>
2014-01-24 23:12:38 +0800

    rails app of statues

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = '4774a8aa79a3f9f2e5f29b69f0bb3935acca65a864eddbf075a3dd2edb7167d04ec46ee03f271917f7b651e2b59980efc290f7ea3791e61d76c4fb2c3ae472e9'
